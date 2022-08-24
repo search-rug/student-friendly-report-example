5d971b95a1920fbb9dc5d52f5e94360b4e005b4f
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-08-24 13:46:24 +0200

    initialization of the repository from https://github.com/tupac-rocher/student-friendly-pipeline-example

---
+++ src/main/java/com/montpellier/Office.java
@@ -0,0 +1,17 @@
+package com.montpellier;
+
+public class Office {
+    private Computer computer;
+
+    public Office() {
+        computer = new Computer();
+    }
+
+    public void startingToWork () {
+        computer.turnOnComputer();
+    }
+
+    public void endingWork(){
+        computer.turnOffComputer();
+    }
+}
